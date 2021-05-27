a0db99f1470303b9c2fb78acad2d9ce47f3abf01
Brian Mak <eggrollbot@gmail.com>
2021-05-26 22:26:16 -0700

    Added main

---
+++ sorting_comparison.c
@@ -0,0 +1,22 @@
+#include "bubble.h"
+#include "quick.h"
+#include "set.h"
+#include "shell.h"
+#include "sorting_statistics.h"
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// Description:
+// The entry point of the program.
+//
+// Parameters:
+// Nothing.
+//
+// Returns:
+// int - The exit status of the program (0 = success, otherwise error).
+int main( ) {
+	return 0;
+}
